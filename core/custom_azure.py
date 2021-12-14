ac42cd0cfb5d48e34980850a21b695599aede16d
Rishubh Kumar <82128737+Rishubh03@users.noreply.github.com>
2021-12-14 22:00:45 +0530

    inital commit

--- core/custom_azure.py
+++ core/custom_azure.py
@@ -1,13 +1,16 @@
 from storages.backends.azure_storage import AzureStorage
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class AzureMediaStorage(AzureStorage):
     account_name = 'djangoaccountstorage03' # Must be replaced by your <storage_account_name>
-    account_key = 'IOV0vAr3xrefVtdKTiuqce+pzIGzGVtk8uTx//p5aaKRJsChFzx7+63Zh0Y8V/PJ1gMSYL65LZmYA2xXqvGugw==' # Must be replaced by your <storage_account_key>
+    account_key = str(os.getenv('ACCOUNT_KEY1')) # Must be replaced by your <storage_account_key>
     azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
     account_name = 'djangoaccountstorage03' # Must be replaced by your storage_account_name
-    account_key = 'oXO3vjNVZ0y53rPcZCwf8QFXbRUW3uv8FTX/mIgxUkmo7Xar6XVSKfeJYR5jAXkaztnYe1Ytff0EzNUA5qvPlA==' # Must be replaced by your <storage_account_key>
+    account_key = str(os.getenv('ACCOUNT_KEY2')) # Must be replaced by your <storage_account_key>
     azure_container = 'static'
     expiration_secs = None
